9a7754eaea4181c8f5cfda864e05c2d0e63ebb7e
mehdi <dehghani_m@rocketmail.com>
2022-08-05 12:53:37 +0430

    continue project

--- app/src/main/java/com/example/flowproject/MainActivity.kt
+++ app/src/main/java/com/example/flowproject/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.flowproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.flowproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLiveData.setOnClickListener {
+
+        }
+
+        binding.btnStateFlow.setOnClickListener {
+
+        }
+
+        binding.btnFlow.setOnClickListener {
+
+        }
+
+        binding.btnShareteFlow.setOnClickListener {
+
+        }
     }
 }
